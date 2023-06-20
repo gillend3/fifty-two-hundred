68071b06b691ceeb250819352ace78ff5b3e8cd1
gillend3 <136641449+gillend3@users.noreply.github.com>
2023-06-20 18:50:45 +0100

    Update word-counter-script.js

--- word-counter-script.js
+++ word-counter-script.js
@@ -1,47 +1,33 @@
-// Maximum word limit
-const MAX_WORDS = 200;
+const textInput = document.getElementById("textInput");
+const wordCountElement = document.getElementById("wordCount");
+const wordList = document.getElementById("wordList");
 
-// Maximum unique word limit
-const MAX_UNIQUE_WORDS = 50;
+let wordCount = 0;
+let uniqueWords = new Set();
 
-// Sample text for testing
-const sampleText = "This is a sample text. This text contains some words that are repeated. The word counter should count unique words.";
+function updateWordCount() {
+  const text = textInput.value;
+  const words = text.match(/\b\w+\b/g) || [];
+  const currentWordCount = words.length;
 
-function updateWordCounter(text) {
-  const trimmedText = text.trim();
-
-  // Count total words
-  const wordCount = trimmedText === '' ? 0 : trimmedText.split(/\s+/).length;
-  console.log(`Word Count: ${wordCount}`);
-
-  // Count unique words
-  const words = trimmedText.toLowerCase().match(/\b\w+\b/g) || [];
-  const uniqueWords = [...new Set(words)];
-
-  // Check if maximum unique word limit is reached
-  if (uniqueWords.length > MAX_UNIQUE_WORDS) {
-    text = removeExcessWords(text, words, uniqueWords);
+  if (currentWordCount > 200) {
+    textInput.value = text.substr(0, 200);
+    return;
   }
 
-  // Display unique words in alphabetical order
-  uniqueWords.sort().forEach((word, index) => {
-    console.log(`${index + 1}. ${word}`);
-  });
+  if (currentWordCount > wordCount) {
+    // Update word count
+    wordCount = currentWordCount;
+    wordCountElement.textContent = "Word Count: " + wordCount;
 
-  // Check if maximum word limit is reached
-  if (wordCount >= MAX_WORDS) {
-    text = text.substr(0, MAX_WORDS);
+    // Check for new unique words
+    words.slice(wordCount - 1).forEach((word) => {
+      if (!uniqueWords.has(word) && uniqueWords.size < 50) {
+        uniqueWords.add(word);
+        const listItem = document.createElement("li");
+        listItem.textContent = word;
+        wordList.appendChild(listItem);
+      }
+    });
   }
-
-  console.log('Updated text:', text);
 }
-
-// Helper function to remove excess words
-function removeExcessWords(text, words, uniqueWords) {
-  const excessWords = uniqueWords.slice(MAX_UNIQUE_WORDS);
-  const regex = new RegExp(`\\b(${excessWords.join('|')})\\b`, 'gi');
-  return text.replace(regex, '');
-}
-
-// Example usage
-updateWordCounter(sampleText);
