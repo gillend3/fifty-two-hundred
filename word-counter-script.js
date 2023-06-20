f3d1f19826839e299bc772a9d9ced3846a989c23
gillend3 <136641449+gillend3@users.noreply.github.com>
2023-06-20 18:58:47 +0100

    Update word-counter-script.js

--- word-counter-script.js
+++ word-counter-script.js
@@ -7,20 +7,20 @@ let uniqueWords = new Set();
 
 function updateWordCount() {
   const text = textInput.value;
-  const words = text.match(/\b\w+\b/g) || [];
+  const words = text.trim().split(/\s+/);
   const currentWordCount = words.length;
 
   if (currentWordCount > 200) {
-    textInput.value = text.substr(0, 200);
-    return;
+    const trimmedWords = words.slice(0, 200);
+    textInput.value = trimmedWords.join(" ");
   }
 
-  if (currentWordCount > wordCount) {
-    // Update word count
-    wordCount = currentWordCount;
-    wordCountElement.textContent = "Word Count: " + wordCount;
+  // Update word count
+  wordCount = Math.min(currentWordCount, 200);
+  wordCountElement.textContent = "Word Count: " + wordCount;
 
-    // Check for new unique words
+  // Check for new unique words
+  if (currentWordCount > wordCount) {
     words.slice(wordCount - 1).forEach((word) => {
       if (!uniqueWords.has(word) && uniqueWords.size < 50) {
         uniqueWords.add(word);
